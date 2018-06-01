86a55cdaafbc95dd51968aa970879e43e51e23e8
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-06-01 11:37:17 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,39 @@
 export let state;
+// pets: [{name: 'Splash', type: 'turtle', id: 100}
 
-
-export function managePets(){
+//pet: {name: 'avalanche', species: 'puppy', id: 101}}
+export function managePets(state = { pets: [] }, action) {
+  switch (action.type) {
+    case "ADD_PET":
+      let copy = { ...state, pets: [...state.pets] };
+      copy.pets.push(action.pet);
+      return copy;
+    case "REMOVE_PET":
+      // let copy = { ...state, pets: [...state.pets] };
+      let filteredPets = state.pets.filter(pet => pet.id !== action.id);
+      return { pets: filteredPets };
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action) {
+  state = managePets(state, action);
+  render();
 }
 
-export function render(){
-
+export function render() {
+  let outputString = "";
+  state.pets.forEach((pet, index) => {
+    if (index === 0) {
+      // if index 0 add <ul>
+      outputString += "<ul>";
+    }
+    outputString += `<li>${pet.name}</li>`;
+    if (index === state.pets.length - 1) {
+      // if last index add </ul>
+      outputString += "</ul>";
+    }
+  });
+  document.getElementById("container").innerHTML = outputString;
 }
